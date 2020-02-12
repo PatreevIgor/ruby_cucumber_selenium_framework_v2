7bb8f171d6db635ebcabf5981b25325f9b31c073
igor.patreew <igor.patreew@activeplatform.com>
2020-02-12 14:24:30 +0300

    Restructure foldiers and add feature specs

---
+++ spec/features/google/check_main_google_page_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'check_main_google_page.feature' do
+  context 'reusing @tag' do
+    # Add code
+  end
+
+  context 'сheck @tag on others virtual machines' do
+    # Add code
+  end
+end
